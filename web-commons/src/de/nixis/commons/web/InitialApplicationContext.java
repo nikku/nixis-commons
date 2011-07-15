c7bc5b3d2249f1350c233820b8340443d859d963
Nikku <git_nikku@nixis.de>
2011-07-15 10:52:53 +0000

    git-svn-id: https://svn.nixis.de/nixis-commons/trunk@8 1a5e26a7-e170-45da-b526-4b64c1c902a6

--- web-commons/src/de/nixis/commons/web/InitialApplicationContext.java
+++ web-commons/src/de/nixis/commons/web/InitialApplicationContext.java
@@ -9,16 +9,18 @@ import javax.naming.NamingException;
  * This is a utility class which provides methods to
  * lookup beans, the configuration and loggers for all classes
  * in this project
- * 
+ *
  * @author Dummdoedel
  */
 public class InitialApplicationContext {
+
+    private static String applicationName;
 
     private static InitialContext ix;
     private static Logger logger;
 
     private static Configuration configuration;
-    
+
     /**
      * Return configuration
      * @return
@@ -27,19 +29,19 @@ public class InitialApplicationContext {
         if (configuration == null) configuration = lookupBean(Configuration.class);
         return configuration;
     }
-    
+
     /**
      * Look up logger for specified class
      * whether or not debugging is enabled for this class depends on a
      * [full-class-name].debug property set in configuration
-     * 
+     *
      * @param clazz
      * @return logger for specified class
      */
     public static Logger getLogger(Class clazz) {
         return lookupLogger(clazz, true);
     }
-    
+
     /**
      * Get logger for this context. It is per se in debugmode, since
      * the configuration might not yet have been loaded
@@ -52,12 +54,27 @@ public class InitialApplicationContext {
 
     /**
      * Look up logger for specified class.
-     * Second parameter specifies 
+     * Second parameter specifies
      * @return
      */
-    private static Logger lookupLogger(Class clazz, boolean initFromConfig) {        
+    private static Logger lookupLogger(Class clazz, boolean initFromConfig) {
         boolean debug = (!initFromConfig ? true : getConfig().getProperty(clazz.getName() + ".debug", false, true));
         return new Logger(clazz, debug);
+    }
+
+    /**
+     * Look up bean with the specified interface. This method will automatically
+     * append <code>Bean</code> to the interface name.
+     *
+     * When using the default naming, use
+     * InitialApplicationContext.lookupStatefulBean to obtain stateful beans.
+     *
+     * @param <T>
+     * @param clazz
+     * @return
+     */
+    public static <T> T lookupBean(Class<T> clazz) {
+        return lookupBean(clazz, applicationName + "/" + clazz.getSimpleName() + "Bean/local");
     }
 
     /**
@@ -66,13 +83,13 @@ public class InitialApplicationContext {
      * @param clazz
      * @return
      */
-    public static <T> T lookupBean(Class<T> clazz) {
-        
+    public static <T> T lookupBean(Class<T> clazz, String jndiName) {
+        if (applicationName == null) throw new RuntimeException("[InitialApplicationContext] Application Name is not configured");
+
         try {
-            System.out.println("MOOOOOOOOOOOOOOP " + getContext().getNameInNamespace());
-            return (T) getContext().lookup("anog.small.rl/" + clazz.getSimpleName() + "/local");
+            return (T) getContext().lookup(jndiName);
         } catch (NamingException ne) {
-            getLogger().fatal("Unable to load " + clazz.getSimpleName(), ne); 
+            getLogger().fatal("Unable to load " + clazz.getSimpleName(), ne);
             throw new RuntimeException(ne);
         } catch(ClassCastException cce) {
             getLogger().fatal("Unable to load " + clazz.getSimpleName(), cce);
@@ -88,5 +105,15 @@ public class InitialApplicationContext {
     private static InitialContext getContext() throws NamingException {
         if (ix == null) ix = new InitialContext();
         return ix;
+    }
+
+    /**
+     * Sets the namespace used to find beans (such as <b>VB</b> if beans can
+     * be found under VB/BeanName/local).
+     *
+     * @param applicationName
+     */
+    public static void setApplicationNamespace(String applicationName) {
+        InitialApplicationContext.applicationName = applicationName;
     }
 }
